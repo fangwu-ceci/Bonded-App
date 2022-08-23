29f88a9f588c39ba4b0885b73d00ff07c6850066
Tommy Fenwick <tommyfenwick9@hotmail.com>
2022-08-23 21:31:12 +0100

    making some progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Group.delete_all
+
+10.times do
+  Group.create!(
+    name: Faker::Team.name,
+    about: "Fun and friendly group for all to join",
+    location: Faker::Address.city,
+    category: Faker::Hobby.activity
+  )
+end
