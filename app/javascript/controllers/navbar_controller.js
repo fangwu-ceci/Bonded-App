3bb07fd1b32a8679554613c4c21b9d1eecae5f38
Cecilia Wu <fangwu.cecilia@gmail.com>
2022-08-23 22:04:12 +0100

    updated seed

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -6,11 +6,11 @@ export default class extends Controller {
     console.log("hello from the navbar controller")
   }
 
-  stickNavbar() {
-    if (window.scrollY >= window.innerHeight) {
-      this.element.classList.add("navbar-lewagon-sticky")
-    } else {
-      this.element.classList.remove("navbar-lewagon-sticky")
-    }
- }
+//   stickNavbar() {
+//     if (window.scrollY >= window.innerHeight) {
+//       this.element.classList.add("navbar-lewagon-sticky")
+//     } else {
+//       this.element.classList.remove("navbar-lewagon-sticky")
+//     }
+//  }
 }
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,7 @@ Rails.application.routes.draw do
  resources :users, only: [:index, :show] do
     resources :messages, only: :create
   end
+  # resources :chatrooms, only: :show do
+  #   resources :messages, only: :create
+  # end
 end
