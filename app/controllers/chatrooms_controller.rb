6795b7b9a7eb89c3504d0a482e13ae6d718c8618
Cecilia Wu <fangwu.cecilia@gmail.com>
2022-09-06 10:15:25 +0100

    updated all link_to

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -4,4 +4,16 @@ class ChatroomsController < ApplicationController
     @message = Message.new
   end
 
+  def create
+    @chatroom = Chatroom.find(params[:chatroom_id])
+    @message = Message.new(post_params)
+    @message.chatroom = @chatroom
+    @message.user = current_user
+    if @message.save
+      redirect_to group_path(@group)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   resources :users, only: [:index, :show]
-  resources :chatrooms, only: :show do
+  resources :chatrooms, only: [:show, :create] do
     resources :messages, only: :create
   end
   resources :groups, only: [:index, :show, :destroy,] do
