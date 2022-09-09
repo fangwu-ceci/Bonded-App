c79f346d6df75d3df261a4d4f971853f2fe78a66
huda salameh <huda.salameh44@gmail.com>
2022-09-09 18:03:51 +0100

    Minor changes - adjusted flash alert message  of search functionality and capitalised Friends in navbar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
     max_age = params[:max_age]
     gender = params[:gender]
     if params[:commit] && (!params[:min_age].present? || !params[:max_age].present? || !params[:location].present? || !params[:gender].present?)
-      flash[:alert] = "Fill out all the info"
+      flash[:alert] = "Use the filters to search"
       @users = User.all
     elsif params[:min_age].present?
       sql_query = "location ILIKE ? AND gender = ? AND age BETWEEN ? AND ?", "%#{location}%", "#{gender}", min_age, max_age
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,22 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :groups, through: :user_group, class_name: "Group", foreign_key: "user_id"
+
   has_many :posts, dependent: :destroy, class_name: "Post"
+
   has_many :messages, dependent: :destroy
+
   has_one_attached :photo
+
   validates_presence_of :first_name, :last_name, :email, :age, :about, :location
+
   validates :about, length: { minimum: 5 }
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   has_many :usergroups
+
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
 end
