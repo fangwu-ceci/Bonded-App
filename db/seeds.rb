86b9aa3087d995aad85f40729dda1641c7efb318
Cecilia Wu <fangwu.cecilia@gmail.com>
2022-09-10 15:31:52 +0100

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,15 +31,33 @@ Group.destroy_all
 
 puts "creating users..."
 
-user1 = User.create!(first_name: "Bob", last_name: "Smith", nickname: "Bobby", gender: "Male", age: age.sample, location: uk_cities.sample, email: "bobby@smith.com", password: "123456", about: "City professional who wants to find like-minded friends. I love art, theatre and books. Keen to exchange my reading list and form a bookclub.", avatar_url: "https://www.biography.com/.image/t_share/MTgwMjk3MjI5MjU5NTE1MDMw/gettyimages-3315233.jpg")
-user2 = User.create!(first_name: "Jenny", last_name: "Johnson", nickname: "Jen", gender: "Female", age: age.sample, location: uk_cities.sample, email: "jen@johnson.com", password: "123456", about: "I am passionate about empowering women and would like to find other wonder-women to change the world together", avatar_url: "https://discoverymood.com/wp-content/uploads/2020/04/Mental-Strong-Women-min-480x340.jpg")
-user3 = User.create!(first_name: "Alice", last_name: "Brown", nickname: "Ally", gender: "Female", age: age.sample, location: uk_cities.sample, email: "alice@brown.com", password: "123456", about: "A single mum who wants to find other mums to go shopping and do fun things together, not just talk about changing nappies.", avatar_url: "https://i0.wp.com/digital-photography-school.com/wp-content/uploads/2014/11/4-1.jpg?fit=900%2C675&ssl=1")
-user4 = User.create!(first_name: "Amy", last_name: "Jones", nickname: "Amy", gender: "Female", age: age.sample, location: uk_cities.sample, email: "amy@jones.com", password: "123456", about: "I am a freelance wrirter. I love travelling and writing stories. I'd like to find friends to travel together. My next destination is Argentina.", avatar_url: "https://images.unsplash.com/photo-1582610285985-a42d9193f2fd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjl8fHdvbWFufGVufDB8fDB8fA%3D%3D&w=1000&q=80")
-user5 = User.create!(first_name: "Nicolas", last_name: "Johnson", nickname: "Nick", gender: "Male", age: age.sample, location: uk_cities.sample, email: "nick@johnson.com", password: "123456", about: "New to the city, looking to meet locals to do sports, i.e. tennis, rugy and climbing.", avatar_url:"https://hips.hearstapps.com/hbz.h-cdn.co/assets/17/08/terencetelle.png")
-user6 = User.create!(first_name: "Adam", last_name: "Smith", nickname: "Adam", gender: "Male", age: age.sample, location: uk_cities.sample, email: "adam@smith.com", password: "123456", about: "Just arrived at the city and would like to meet more like-minded friends and explore the city together.", avatar_url:"https://media.glamour.com/photos/5b6873f780b8607e43a9ec0e/1:1/w_354%2Cc_limit/river.jpg")
-user7 = User.create!(first_name: "Emma", last_name: "Baker", nickname: "Emma", gender: "Female", age: age.sample, location: uk_cities.sample, email: "emma@baker.com", password: "123456", about: "Keen traveller and explorer, want to find some nice friends to enjoy this beautiful city with.", avatar_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/Gal_Gadot_by_Gage_Skidmore_2.jpg/1200px-Gal_Gadot_by_Gage_Skidmore_2.jpg")
-user8 = User.create!(first_name: "Elizabeth", last_name: "Taylor", nickname: "Lizzy", gender: "Female", age: age.sample, location: uk_cities.sample, email: "lizzy@taylor.com", password: "123456", about: "I am a passionate baker. I love baking lots of cakes in my spare time and would love to find a friend to bake cakes with.", avatar_url: "https://d1vo8zfysxy97v.cloudfront.net/media/blog/abnormal-testosterone-levels-in-women-symptoms-risks-and-helpful-foods_fi_va9185016baf8cec6e6ec901990453bef5ea7d999.jpg")
-user9 = User.create!(first_name: "Oliver", last_name: "Jones", nickname: "Ollie", gender: "Male", age: age.sample, location: uk_cities.sample, email: "oliver@jones.com", password: "123456", about: "Expanding my social circle, meeting like-minded frineds to check out new foodie haunts and do more activities together.", avatar_url:"https://cavemancircus.com/wp-content/uploads/2019/04/ana-maria-nichita-417521-unsplash-2.jpg")
+user1 = User.create!(first_name: "Bob", last_name: "Smith", nickname: "Bobby", gender: "Male", age: 34, location: "London", email: "bobby@smith.com", password: "123456",
+  about: " Just moved to London from New York for work. I work in the city and live near Islington. I love a stroll, a beach, rock music, good food and good movies. Keen to meet more like-minded individuals and find a cool community.",
+  avatar_url: "https://www.biography.com/.image/t_share/MTgwMjk3MjI5MjU5NTE1MDMw/gettyimages-3315233.jpg")
+user2 = User.create!(first_name: "Jenny", last_name: "Johnson", nickname: "Jen", gender: "Female", age: age.sample, location: uk_cities.sample, email: "jen@johnson.com", password: "123456",
+  about: "I am passionate about empowering women and would like to find other wonder-women to change the world together",
+  avatar_url: "https://discoverymood.com/wp-content/uploads/2020/04/Mental-Strong-Women-min-480x340.jpg")
+user3 = User.create!(first_name: "Alice", last_name: "Brown", nickname: "Ally", gender: "Female", age: age.sample, location: uk_cities.sample, email: "alice@brown.com", password: "123456",
+  about: "A single mum who wants to find other mums to go shopping and do fun things together, not just talk about changing nappies.",
+  avatar_url: "https://i0.wp.com/digital-photography-school.com/wp-content/uploads/2014/11/4-1.jpg?fit=900%2C675&ssl=1")
+user4 = User.create!(first_name: "Amy", last_name: "Jones", nickname: "Amy", gender: "Female", age: age.sample, location: uk_cities.sample, email: "amy@jones.com", password: "123456",
+  about: "I am a freelance wrirter. I love travelling and writing stories. I'd like to find friends to travel together. My next destination is Argentina.",
+  avatar_url: "https://images.unsplash.com/photo-1582610285985-a42d9193f2fd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjl8fHdvbWFufGVufDB8fDB8fA%3D%3D&w=1000&q=80")
+user5 = User.create!(first_name: "Nicolas", last_name: "Johnson", nickname: "Nick", gender: "Male", age: age.sample, location: uk_cities.sample, email: "nick@johnson.com", password: "123456",
+  about: "New to the city, looking to meet locals to do sports, i.e. tennis, rugy and climbing.",
+  avatar_url:"https://hips.hearstapps.com/hbz.h-cdn.co/assets/17/08/terencetelle.png")
+user6 = User.create!(first_name: "Adam", last_name: "Smith", nickname: "Adam", gender: "Male", age: 34, location: "London", email: "adam@smith.com", password: "123456",
+  about: "Originally from HongKong, I moved to London before the pandemic. I work in the city and live near Angel. I go to the gym pretty regularly(becaiuse of my love of food!). I am also a movie enthusiast and run a movie group in Bonded.",
+  avatar_url:"https://media.glamour.com/photos/5b6873f780b8607e43a9ec0e/1:1/w_354%2Cc_limit/river.jpg")
+user7 = User.create!(first_name: "Emma", last_name: "Baker", nickname: "Emma", gender: "Female", age: age.sample, location: uk_cities.sample, email: "emma@baker.com", password: "123456",
+  about: "Keen traveller and explorer, want to find some nice friends to enjoy this beautiful city with.",
+  avatar_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/Gal_Gadot_by_Gage_Skidmore_2.jpg/1200px-Gal_Gadot_by_Gage_Skidmore_2.jpg")
+user8 = User.create!(first_name: "Elizabeth", last_name: "Taylor", nickname: "Lizzy", gender: "Female", age: age.sample, location: uk_cities.sample, email: "lizzy@taylor.com", password: "123456",
+  about: "I am a passionate baker. I love baking lots of cakes in my spare time and would love to find a friend to bake cakes with.",
+  avatar_url: "https://d1vo8zfysxy97v.cloudfront.net/media/blog/abnormal-testosterone-levels-in-women-symptoms-risks-and-helpful-foods_fi_va9185016baf8cec6e6ec901990453bef5ea7d999.jpg")
+user9 = User.create!(first_name: "Oliver", last_name: "Jones", nickname: "Ollie", gender: "Male", age: age.sample, location: uk_cities.sample, email: "oliver@jones.com", password: "123456",
+  about: "Expanding my social circle, meeting like-minded frineds to check out new foodie haunts and do more activities together.",
+  avatar_url:"https://cavemancircus.com/wp-content/uploads/2019/04/ana-maria-nichita-417521-unsplash-2.jpg")
 
 Chatroom.create!(name: "Jenny", chatroom_requester_id: 1, chatroom_receiver_id: 2)
 Chatroom.create!(name: "Ally", chatroom_requester_id: 1, chatroom_receiver_id: 3)
@@ -73,10 +91,10 @@ group2 = Group.create!(
 group3 = Group.create!(
   image_url: "https://images.pexels.com/photos/7991119/pexels-photo-7991119.jpeg",
   name: "Movies for All",
-  about: "This Group is a unique collective made up of directors, producers, cinematographers, screen writers, sound designers, photographers,
-  composers, actors, and students that collaborate on a wide range of creative filmmaking projects. Members range the gamut from well known professionals,
-  to students, to those just interested in learning and being a part of a creative community.",
-  location: uk_cities.sample,
+  about: "Join to meet people and see movies!
+  It's really pretty basic; the organiser and assistant organisers pick films, we meet beforehand and generally go for a drink after. It's a relaxed group which people dip in and out of depending on their film genre preference etc, so there are always new people at each meet.
+  We're not film snobs in this group- we see both thoughtful, foreign language films at indie cinemas and 'leave your brain at home' Hollywood blockbusters at multiplexes. Personally it depends on my mood. So if you like films, come along, watch a film and discuss it over a drink.",
+  location: "London",
   category: "Films"
 )
 
